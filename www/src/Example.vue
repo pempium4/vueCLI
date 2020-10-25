54b2387fb03f446d5729628b736e17b99d2a1378
Pempium4 <front.shipper@gmail.com>
2020-10-25 11:51:34 +0200

    End

---
+++ www/src/Example.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="example">
+    <h1>{{ greeting }}</h1>
+    <app-input :msg="greeting"></app-input>
+  </div>
+</template>
+
+<script>
+import Input from "./Input";
+export default {
+  name: "Example",
+  data: function (){
+    return{
+      greeting: 'Ex text'
+    }
+  },
+  components: {
+    'app-input' : Input
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ www/src/Input.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>{{msg}}</h3>
+    <input type="text" :value="msg" @input="changeMSG">
+    <p>{{text}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function (){
+    return{
+      text: ''
+    }
+  },
+  props: [
+    'msg'
+  ],
+  methods: {
+    changeMSG (event){
+      this.msg = event.target.value;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
